2b99e3c046e441bc1788b27e32928e098ffc2f4d
Daniel Waksman <dwaksman@gmail.com>
2018-08-11 17:38:59 -0300

    Auto populate dashboard and toggle working

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
+let server = require('http').Server(app);
+let io = require('socket.io')(server);
+
+require('./lib/socket').initSocketIOEvents(io);
 
 app.use('/static', express.static(__dirname + '/public'));
 app.use('/socket', express.static(__dirname + '/node_modules/socket.io-client/dist'));
@@ -7,30 +11,35 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'pug')
 
 app.get('/', function (req, res) {
-    res.render('menu', {
-        devices: [
-            { class: "macro away lg", name: "Fuera" },
-            { class: "macro morning lg", name: "Mañana" },
-            { class: "macro night lg", name: "Noche" },
-            { class: "switch light", name: "Zaguan" },
-            { class: "switch light", name: "Alero Este" },
-            { class: "switch light", name: "Alero Sur" },
-            { class: "switch light", name: "Dormitorio 1" },
-            { class: "switch light", name: "Dormitorio 2" },
-            { class: "switch fosset", name: "Jardin Frente" },
-            { class: "info energy md", name: "Consumo", data: "0.23" },
-            { class: "info entry", name: "Frente" },
-            { class: "info entry", name: "Fondo" },
-            { class: "info entry", name: "Cocina" },
-            { class: "info lock", name: "Frente" },
-            { class: "info lock", name: "Fondo" },
-            { class: "info lock", name: "Cocina" },
-            { class: "info temp", name: "Comedor", data: "18" },
-            { class: "info temp", name: "Dormitorio", data: "23" }
-        ],
+    
+    // res.render('dashboard', {
+    //     devices: [
+    //         { class: "macro away lg", name: "Fuera" },
+    //         { class: "macro morning lg", name: "Mañana" },
+    //         { class: "macro night lg", name: "Noche" },
+    //         { class: "switch light", name: "Zaguan" },
+    //         { class: "switch light", name: "Alero Este" },
+    //         { class: "switch light", name: "Alero Sur" },
+    //         { class: "switch light", name: "Dormitorio 1" },
+    //         { class: "switch light", name: "Dormitorio 2" },
+    //         { class: "switch fosset", name: "Jardin Frente" },
+    //         { class: "info energy md", name: "Consumo", data: "0.23" },
+    //         { class: "info entry", name: "Frente" },
+    //         { class: "info entry", name: "Fondo" },
+    //         { class: "info entry", name: "Cocina" },
+    //         { class: "info lock", name: "Frente" },
+    //         { class: "info lock", name: "Fondo" },
+    //         { class: "info lock", name: "Cocina" },
+    //         { class: "info temp", name: "Comedor", data: "18" },
+    //         { class: "info temp", name: "Dormitorio", data: "23" }
+    //     ],
+    // });
+
+    res.render('dashboard', {
+        devices: []
     });
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+server.listen(5050, function () {
+    console.log('Example app listening on port 5050!');
 });
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,5 +1,124 @@
 $(document).ready(function(){
-  
 
-    
+  var socket = io();
+
+  var client = {
+    id: "someuniqueidprobablylogintoken",
+    name: "username"
+  }
+
+  socket.emit('client:register', client);
+  socket.emit('client:boards:get');
+
+  socket.on("client:boards:all", (boards) => {
+    boards.forEach(board => {appendBoard(board)});
   })
+  
+  socket.on("client:board:new", (board) => {
+    appendBoard(board);
+  })
+
+  socket.on("client:board:disable", (id) => {
+    var actuators = $(`[id^='${id}']`);
+    actuators.addClass('disabled');  
+  })
+
+  socket.on("client:board:enable", (id) => {
+    var actuators = $(`[id^='${id}']`);
+    actuators.removeClass('disabled');
+  })
+
+  
+  $(".switch").on('click', switchClick);
+  $(".info").on('click', infoClick);
+  $(".macro").on('click', macroClick);
+
+  function switchClick (e) {
+    e.preventDefault();
+    
+    if ($(this).hasClass('disabled')) return;
+
+    $(this).toggleClass('on');
+    $(".macro").removeClass('active');
+
+    socket.emit("client:switch:actuate", $(this).attr('id'));
+  } 
+
+  function macroClick (e) {
+    e.preventDefault();
+    
+    if ($(this).hasClass('disabled')) return;
+
+    $(this).toggleClass('active');
+  
+    if ($(this).hasClass('active')) {
+      $(".macro").not(this).removeClass('active');
+    }
+  }
+
+  function infoClick (e) {
+    e.preventDefault();
+
+    if ($(this).hasClass('disabled')) return;
+    
+    $(this).toggleClass('active');
+  }
+
+  function appendBoard (board) {
+    console.log(board);
+    let controls = $("#controls");
+
+    if (board.module_0_type != "empty") {
+      var $module_0 = $("<div>", {id: `${board.id}_module_0`, "class": `${board.module_0_type}`});
+
+      if (board.status == "offline") $module_0.addClass('disabled');
+      
+      $(`<span>${board.module_0_name}</span>`)
+        .addClass("name")
+        .appendTo($module_0);
+
+      $module_0.on('click', switchClick);
+      controls.append($module_0);
+    }
+
+    if (board.module_1_type != "empty") {
+      var $module_1 = $("<div>", {id: `${board.id}_module_1`, "class": `${board.module_1_type}`});
+      
+      if (board.status == "offline") $module_1.addClass('disabled');
+
+      $(`<span>${board.module_1_name}</span>`)
+        .addClass("name")
+        .appendTo($module_1);
+
+      $module_1.on('click', switchClick);
+      controls.append($module_1);
+    }
+
+    if (board.module_2_type != "empty") {
+      var $module_2 = $("<div>", {id: `${board.id}_module_2`, "class": `${board.module_2_type}`});
+
+      if (board.status == "offline") $module_2.addClass('disabled');
+
+      $(`<span>${board.module_2_name}</span>`)
+        .addClass("name")
+        .appendTo($module_2);
+
+      $module_2.on('click', switchClick);
+      controls.append($module_2);
+    }
+
+    if (board.module_3_type != "empty") {
+      var $module_3 = $("<div>", {id: `${board.id}_module_3`, "class": `${board.module_3_type}`});
+      
+      if (board.status == "offline") $module_3.addClass('disabled');
+
+      $(`<span>${board.module_3_name}</span>`)
+        .addClass("name")
+        .appendTo($module_3);
+
+      $module_3.on('click', switchClick);
+      controls.append($module_3);
+    }
+  }
+
+})
