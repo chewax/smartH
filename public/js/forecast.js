2b0f39624c2fff493b5105f4ab77bddcc8c81a61
Daniel Waksman <dwaksman@gmail.com>
2021-07-15 17:33:28 -0300

    changed layout and styles

--- public/js/forecast.js
+++ public/js/forecast.js
@@ -4,22 +4,12 @@ $(document).ready(function(){
 
 
     socket.on("console:forecast:get", (data) => {
+
         let $today = $(".forecast .today");
         $today.addClass(data.daily.data[0].icon);
-        $(`<span>${Math.round(data.daily.data[0].temperatureMax)}℃ / ${Math.round(data.daily.data[0].temperatureMin)}℃</span>`)
+        $(`<span>${Math.round(data.daily.data[0].temperatureMax)}°/${Math.round(data.daily.data[0].temperatureMin)}°</span>`)
             .addClass("temp")
             .appendTo($today);
-    
-    
-        let $tomorrow = $(".forecast .tomorrow");
-        $tomorrow.addClass(data.daily.data[1].icon);
-        $(`<span>${Math.round(data.daily.data[1].temperatureMax)}℃ / ${Math.round(data.daily.data[1].temperatureMin)}℃</span>`)
-          .addClass("temp")
-          .appendTo($tomorrow);
-    
-        $(`<span>${Math.round(data.daily.data[1].precipProbability)}%</span>`)
-          .addClass("rain")
-          .appendTo($tomorrow);
     })
 
     let forecast = $("#forecast")
