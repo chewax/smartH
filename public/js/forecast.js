3b5c05d78641bcafb0b6c354270f216b76328f1f
Daniel <dwaksman@gmail.com>
2021-07-26 16:29:56 -0300

    WIP

--- public/js/forecast.js
+++ public/js/forecast.js
@@ -1,18 +1,30 @@
 $(document).ready(function(){
-    var socket = io();
+    let socket = io();
     socket.emit('console:forecast:get');
 
 
-    socket.on("console:forecast:get", (data) => {
+    let iconsMap = {
+        "clear-day": "wi-day-sunny"
+    };
 
-        let $today = $(".forecast .today");
-        $today.addClass(data.daily.data[0].icon);
+    socket.on("console:forecast:get", (data) => {
+        // <i class="wi wi-night-sleet"></i>
+        
+        console.log(data.daily.data[0].icon);
+
+        let $forecast = $(".forecast");
+
+         $(`<i class="wi ${iconsMap[data.daily.data[0].icon]}"></i>`)
+            .appendTo($forecast);
+
+        // $today.addClass( iconsMap[data.daily.data[0].icon] );
         $(`<span>${Math.round(data.daily.data[0].temperatureMax)}°/${Math.round(data.daily.data[0].temperatureMin)}°</span>`)
             .addClass("temp")
-            .appendTo($today);
+            .appendTo($forecast);
     })
 
     let forecast = $("#forecast")
+
 
     
 })
